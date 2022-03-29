51317147822c8dec6e3c1dd242459904414f1144
jyotisormare <sormarejyoti@gmail.com>
2022-03-29 17:14:07 +0530

    Change Controller

--- Microservices-product/src/main/java/com/example/demo/controller/UserController.java
+++ Microservices-product/src/main/java/com/example/demo/controller/UserController.java
@@ -1,9 +1,12 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.example.demo.service.ProductService;
 
 public class UserController {
 
+	@Autowired
 	private ProductService service;
 	
 	public void getString() {
