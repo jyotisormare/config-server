1e7998deedf448c14f08c34a4c360c940b088a0d
jyotisormare <sormarejyoti@gmail.com>
2022-03-29 17:32:17 +0530

    Service impl add

--- Microservices-product/src/main/java/com/example/demo/controller/UserController.java
+++ Microservices-product/src/main/java/com/example/demo/controller/UserController.java
@@ -1,11 +1,12 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.client.RestTemplate;
 
 import com.example.demo.service.ProductService;
 
 public class UserController {
-
+RestTemplate rest;
 	@Autowired
 	private ProductService service;
 	
